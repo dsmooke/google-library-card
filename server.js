d8b30fb119aaa34718ea5c16738287343dc3e97d
Danny Smooke <dana.smooke@gmail.com>
2021-04-05 15:09:45 -0400

    added mongoose connection and required pkgs

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+// const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,13 +13,24 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Add routes, both API and view  // Define API routes here
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("   *** Connected to Database ***"))
+  .catch((err) => console.log(err));
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// }); @audit-issue remove after define api routes
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
